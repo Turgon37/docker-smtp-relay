57486ff8c55a0aa63f44ef08174fa74f67d648a8
Pierre GINDRAUD <pgindraud@gmail.com>
2018-02-03 22:53:25 +0100

    add travic ci

---
+++ tests.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+## Local settings
+build_tags_file="${PWD}/build.sh~tags"
+docker_run_options='--detach'
+
+## Settings initialization
+set -e
+set -x
+
+source ${PWD}/_tools.sh
+
+## Tests
+
+#1 Test build successful
+echo '-> 1 Test build successful'
+[ -f "${build_tags_file}" ]
+
+# Get main image
+echo '-> Get main image'
+image=`head --lines=1 "${build_tags_file}"`
+
+#2 Test if GLPI successfully installed
+echo '-> 2 Test if postfix successfully installed'
+image_name=postfix_2
+docker run --rm $docker_run_options --name "${image_name}" "${image}" which postfix
