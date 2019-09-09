ebdf4425ec14e9c98de3cb14f3494fa4fcd84a90
Pierre GINDRAUD <pgindraud@gmail.com>
2019-09-09 22:32:53 +0200

    Rewrite condition in pure bash syntax in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,20 +31,20 @@ postconf -e "relayhost = ${RELAY_HOST}"
 postconf -e "relay_domains = ${RELAY_DOMAINS}"
 
 # Static restrictions for smtp clients
-if [ "${RELAY_MODE}" = 'STRICT' ]; then
+if [[ "${RELAY_MODE}" = 'STRICT' ]]; then
 # set STRICT mode
 # no one can send mail to another domain than the relay domains list
 # only network/sasl authenticated user can send mail through relay
   postconf -e 'smtpd_relay_restrictions = reject_unauth_destination, permit_sasl_authenticated, permit_mynetworks, reject'
-elif [ "${RELAY_MODE}" = 'ALLOW_SASLAUTH_NODOMAIN' ]; then
+elif [[ "${RELAY_MODE}" = 'ALLOW_SASLAUTH_NODOMAIN' ]]; then
 # set ALLOW_SASLAUTH_NODOMAIN mode
 # only authenticated smtp users can send email to another domain than the relay domains list
   postconf -e 'smtpd_relay_restrictions = permit_sasl_authenticated, reject_unauth_destination, permit_mynetworks, reject'
-elif [ "${RELAY_MODE}" = 'ALLOW_NETAUTH_NODOMAIN' ]; then
+elif [[ "${RELAY_MODE}" = 'ALLOW_NETAUTH_NODOMAIN' ]]; then
 # set ALLOW_NETAUTH_NODOMAIN mode
 # only authenticated smtp users can send email to another domain than the relay domains list
   postconf -e 'smtpd_relay_restrictions = permit_mynetworks, reject_unauth_destination, permit_sasl_authenticated, reject'
-elif [ "${RELAY_MODE}" = 'ALLOW_AUTH_NODOMAIN' ]; then
+elif [[ "${RELAY_MODE}" = 'ALLOW_AUTH_NODOMAIN' ]]; then
 # set ALLOW_AUTH_NODOMAIN mode
 # no one can send mail to another domain than the relay domains list
 # only network/sasl authenticated user can send mail through relay
@@ -56,33 +56,33 @@ fi
 
 
 # Set hostname
-if [ -n "${RELAY_MYHOSTNAME}" ]; then
+if [[ -n "${RELAY_MYHOSTNAME}" ]]; then
   postconf -e "myhostname = ${RELAY_MYHOSTNAME}"
 fi
 
 # Set default postmaster value
-if [ -z "$RELAY_POSTMASTER" ]; then
+if [[ -z "$RELAY_POSTMASTER" ]]; then
   RELAY_POSTMASTER="postmaster@${RELAY_MYDOMAIN}"
 fi
 postconf -e "2bounce_notice_recipient = ${RELAY_POSTMASTER}"
 
 # Update the sender mapping databases
-if [ -f /etc/postfix/sender_canonical ]; then
+if [[ -f /etc/postfix/sender_canonical ]]; then
   postconf -e "sender_canonical_maps = hash:/etc/postfix/sender_canonical"
   postmap /etc/postfix/sender_canonical
 fi
 
 # Update the aliases database
 aliases=$(postconf alias_maps |cut -d ':' -f 2)
-if [ -f $aliases ]; then
+if [[ -f $aliases ]]; then
   newaliases
 fi
 
 # Configure authentification to relay if needed
-if [ \( -n "${RELAY_LOGIN}" -a -n "${RELAY_PASSWORD}" \) -o \( -f /etc/postfix/sasl_passwd \) ]; then
+if [[ \( -n "${RELAY_LOGIN}" && -n "${RELAY_PASSWORD}" \) || \( -f /etc/postfix/sasl_passwd \) ]]; then
   postconf -e 'smtp_sasl_auth_enable = yes'
   # use password from hash database
-  if [ -n "${RELAY_LOGIN}" -a -n "${RELAY_PASSWORD}" ]; then
+  if [[ -n "${RELAY_LOGIN}" && -n "${RELAY_PASSWORD}" ]]; then
    # use static database
     postconf -e "smtp_sasl_password_maps = inline:{${RELAY_HOST}=${RELAY_LOGIN}:${RELAY_PASSWORD}}"
   else
@@ -91,7 +91,7 @@ if [ \( -n "${RELAY_LOGIN}" -a -n "${RELAY_PASSWORD}" \) -o \( -f /etc/postfix/s
   fi
   postconf -e 'smtp_sasl_security_options = noanonymous'
 
-  if [ -n "${RELAY_USE_TLS}" -a "${RELAY_USE_TLS}" = 'yes' -a -z "${RELAY_TLS_CA}" ]; then
+  if [[ "${RELAY_USE_TLS}" = 'yes' && -z "${RELAY_TLS_CA}" ]]; then
     echo "you must fill RELAY_TLS_CA with the path to the CA file in the container" >&2
     exit 1
   fi
