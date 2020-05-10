5300ef47efc6b34b79de1d4a8a56bb9e770ff0d3
Pierre GINDRAUD <pgindraud@gmail.com>
2020-05-10 18:59:48 +0200

    Add sasl client auth test

--- tests.sh
+++ tests.sh
@@ -39,38 +39,77 @@ source "${PWD}/_tools.sh"
 # shellcheck disable=SC2038
 find . -name '*.sh' | xargs shellcheck docker-entrypoint.d/*
 
+
+
 # Image tests
 ./container-structure-test \
     test --image "${image_building_name}" --config ./tests.yml
 
 
+
 #2 Test timezone setting
 echo '-> 2 Test timezone'
+
 image_name=smtp_2
+
 docker run $docker_run_options --name "${image_name}" --env='TZ=Europe/Paris' "${image_building_name}"
 wait_for_string_in_container_logs "${image_name}" 'connect from localhost'
 # test
 if ! [[ $(docker exec "${image_name}" readlink -f /etc/localtime) =~ Europe/Paris$ ]]; then
   docker logs "${image_name}"
+  echo 'The timezone setting has failed' 1>&2
+  stop_and_remove_container "${image_name}"
   false
 fi
 stop_and_remove_container "${image_name}"
 
 
-#2 Test sasl_client feed
+
+#3 Test sasl_client feed
 echo '-> 3 Test sasl_client feed'
+
 image_name=smtp_3
 cat >/tmp/client_sasl_passwd <<EOF
 user1 password1
 user2 password2
 user3 password3
 EOF
+
 docker run $docker_run_options --name "${image_name}" -v /tmp/client_sasl_passwd:/etc/postfix/client_sasl_passwd "${image_building_name}"
 wait_for_string_in_container_logs "${image_name}" 'connect from localhost'
 # test
 if ! docker exec "${image_name}" test -f /data/sasldb2 && \
      [[ $(docker exec "${image_name}" /opt/listpasswd.sh | wc -l) == $(wc -l < /tmp/client_sasl_passwd) ]]; then
   docker logs "${image_name}"
+  echo 'The client_sasl_passwd feed has failed' 1>&2
+  stop_and_remove_container "${image_name}"
+  false
+fi
+stop_and_remove_container "${image_name}"
+
+
+
+#4 Test sasl_client auth
+echo '-> 4 Test sasl_client auth'
+
+image_name=smtp_4
+cat >/tmp/client_sasl_passwd <<EOF
+user1 password1
+EOF
+
+docker run $docker_run_options --name "${image_name}" -v /tmp/client_sasl_passwd:/etc/postfix/client_sasl_passwd "${image_building_name}"
+wait_for_string_in_container_logs "${image_name}" 'connect from localhost'
+# test
+if ! docker exec "${image_name}" /opt/smtp_client.py -s test -f noreply@domain.com admin@domain.com --user user1:password1; then
+  docker logs "${image_name}"
+  echo 'The success SASL login test has failed' 1>&2
+  stop_and_remove_container "${image_name}"
+  false
+fi
+if docker exec "${image_name}" /opt/smtp_client.py -s test -f noreply@domain.com admin@domain.com --user user1:badpassword; then
+  docker logs "${image_name}"
+  echo 'The fail SASL login test has failed' 1>&2
+  stop_and_remove_container "${image_name}"
   false
 fi
 stop_and_remove_container "${image_name}"
